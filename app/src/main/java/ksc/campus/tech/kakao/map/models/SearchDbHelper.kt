a3e146a049795a6bbde852939e8359b3dac5ef4a
KSC <ksc1008@naver.com>
2024-07-08 15:44:40 +0900

    feat: removed methods about SearchResultTable

--- app/src/main/java/ksc/campus/tech/kakao/map/models/SearchDbHelper.kt
+++ app/src/main/java/ksc/campus/tech/kakao/map/models/SearchDbHelper.kt
@@ -14,12 +14,7 @@ data class SearchResult(val name: String, val address: String, val type: String)
 class SearchDbHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(SearchResultContract.CREATE_QUERY)
         db?.execSQL(SearchKeywordContract.CREATE_QUERY)
-
-        if (db != null) {
-            insertInitialData(db)
-        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -30,32 +25,9 @@ class SearchDbHelper(context: Context) :
             db?.execSQL(SearchKeywordContract.DROP_QUERY)
             db?.execSQL(SearchKeywordContract.CREATE_QUERY)
         }
-    }
-
-    private fun insertInitialData(db: SQLiteDatabase) {
-        val dataList = InitialDataset.initialData
-        for (data in dataList) {
-            val contentValues = ContentValues().apply {
-                put(SearchResultContract.COLUMN_NAME, data.name)
-                put(SearchResultContract.COLUMN_ADDRESS, data.address)
-                put(SearchResultContract.COLUMN_TYPE, data.type)
-            }
-            db.insert(SearchResultContract.TABLE_NAME, null, contentValues)
+        if(oldVersion in 1 .. 3){
+            db?.execSQL(SearchResultContract.DROP_QUERY)
         }
-    }
-
-    fun insertSearchResult(name: String, address: String, type: String) {
-        val db = writableDatabase
-        val contentValues = ContentValues().apply {
-            put(SearchResultContract.COLUMN_NAME, name)
-            put(SearchResultContract.COLUMN_ADDRESS, address)
-            put(SearchResultContract.COLUMN_TYPE, type)
-        }
-        db.insert(SearchResultContract.TABLE_NAME, null, contentValues)
-    }
-
-    fun insertSearchResult(searchResult: SearchResult) {
-        insertSearchResult(searchResult.name, searchResult.address, searchResult.type)
     }
 
     fun insertOrReplaceKeyword(keyword: String) {
@@ -67,31 +39,6 @@ class SearchDbHelper(context: Context) :
         db.replace(SearchKeywordContract.TABLE_NAME, null, contentValues)
     }
 
-    fun updateSearchResult(id: String, name: String, address: String, type: String) {
-        val db = writableDatabase
-        val contentValues = ContentValues().apply {
-            put(BaseColumns._ID, id)
-            put(SearchResultContract.COLUMN_NAME, name)
-            put(SearchResultContract.COLUMN_ADDRESS, address)
-            put(SearchResultContract.COLUMN_TYPE, type)
-        }
-        db.update(
-            SearchResultContract.TABLE_NAME,
-            contentValues,
-            "${BaseColumns._ID} = ?",
-            arrayOf(id)
-        )
-    }
-
-    fun updateSearchResult(id: String, searchResult: SearchResult) {
-        updateSearchResult(id, searchResult.name, searchResult.address, searchResult.type)
-    }
-
-    fun deleteSearchResult(id: String) {
-        val db = writableDatabase
-        db.delete(SearchResultContract.TABLE_NAME, "${BaseColumns._ID} = ?", arrayOf(id))
-    }
-
     fun deleteKeyword(keyword: String) {
         val db = writableDatabase
         db.delete(
@@ -99,30 +46,6 @@ class SearchDbHelper(context: Context) :
             "${SearchKeywordContract.COLUMN_KEYWORD} = ?",
             arrayOf(keyword)
         )
-    }
-
-    private fun getAllSearchResultFromCursor(cursor: Cursor?): List<SearchResult> {
-        val result = mutableListOf<SearchResult>()
-
-        try {
-            while (cursor?.moveToNext() == true) {
-                result.add(
-                    SearchResult(
-                        cursor.getString(1),
-                        cursor.getString(2),
-                        cursor.getString(3)
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            if (cursor != null && !cursor.isClosed) {
-                cursor.close()
-            }
-        }
-
-        return result
     }
 
     private fun getAllSearchKeywordFromCursor(cursor: Cursor?): List<String> {
@@ -141,23 +64,6 @@ class SearchDbHelper(context: Context) :
         }
 
         return result
-    }
-
-    fun queryAllSearchResults(): List<SearchResult> {
-        val db = readableDatabase
-        val cursor = db.rawQuery("SELECT * FROM ${SearchResultContract.TABLE_NAME}", null)
-
-        return getAllSearchResultFromCursor(cursor)
-    }
-
-    fun querySearchResultsByName(name: String): List<SearchResult> {
-        val db = readableDatabase
-        val cursor = db.rawQuery(
-            "SELECT * FROM ${SearchResultContract.TABLE_NAME} WHERE ${SearchResultContract.COLUMN_NAME} LIKE \"%$name%\"",
-            null
-        )
-
-        return getAllSearchResultFromCursor(cursor)
     }
 
     fun queryAllSearchKeywords(): List<String> {
